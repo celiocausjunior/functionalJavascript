43c96716ac2b20b2c6e5d9b66b372d29345b253e
Celio Junior <celiocausjunior@gmail.com>
2023-03-16 14:41:32 -0300

    Promise 1

---
+++ promise.js
@@ -0,0 +1,14 @@
+
+
+const promise = new Promise(function(cumprirPromessa){
+    cumprirPromessa(3)
+}).then((value)=> console.log(value))
+
+const promise2 = new Promise(function(resolve){
+    resolve(['Ana', 'Bia', 'Carlos'])
+})
+.then((value)=> value[0])
+//.then((value)=> console.log(value))
+.then((value)=> value[0])
+.then((value)=> console.log(value))
+
