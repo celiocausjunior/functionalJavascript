8b928c710c3d21b706e4c925199216a6d0faf24e
Celio Junior <celiocausjunior@gmail.com>
2023-03-16 16:44:31 -0300

    Desafio 3

---
+++ desafio3.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const path = require('path')
+
+const caminho = path.join(__dirname, 'dados.txt')
+
+function exibirConteudo(caminho) {
+    return new Promise(resolve =>{
+       fs.readFile(caminho, function (_, conteudo){
+        resolve(conteudo.toLocaleString())
+        })
+    })
+    
+}
+
+exibirConteudo(caminho).then(console.log)
+
+/* console.log('Inicio...')
+fs.readFile(caminho, exibirConteudo)
+fs.readFile(caminho, (_, conteudo) => console.log(conteudo.toString()))
+console.log('Fim...')
+ */
