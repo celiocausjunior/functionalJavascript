4adcdfaf08c8bdea988da5d3425a0ddc7af4d218
Celio Junior <celiocausjunior@gmail.com>
2023-03-16 16:25:29 -0300

    Promise 3

--- promise.js
+++ promise.js
@@ -1,4 +1,4 @@
-
+/* 
 
 const promise = new Promise(function(resolve){
     resolve(3)
@@ -12,3 +12,41 @@ const promise2 = new Promise(function(resolve){
 .then((value)=> value[0])
 .then((value)=> console.log(value))
 
+
+setTimeout(function(){
+    console.log('Exceutando callback')
+
+    setTimeout(function(){
+        console.log('Exceutando callback 2')
+
+        setTimeout(function(){
+            console.log('Executando callback 3')
+        },2000)
+    },2000)
+},2000) */
+
+/* function timeOut(time=2000){
+    return new Promise(function(resolve){
+        setTimeout(function(){
+            console.log('Executando')
+            resolve()
+        }, time)
+    })
+}
+
+timeOut()
+    .then(()=>timeOut())
+    .then(timeOut) */
+
+    function gerarNumeros(min,max){
+        if(min>max){
+            [max,min] = [min,max]
+        }
+        return new Promise(resolve =>{
+            const random = parseInt(Math.random() * (max - min + 1) + min)
+            resolve(random)
+        })
+
+    }
+
+    gerarNumeros(10,30).then(num => console.log(num))
